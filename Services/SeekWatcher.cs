234b4cb81de89b77a875bdfe5418f2557bcab99e
Jack Hogan <jackhogan11@gmail.com>
2025-02-26 01:21:59 +0000

    Finished initial feature set

--- Services/SeekWatcher.cs
+++ Services/SeekWatcher.cs
@@ -7,6 +7,7 @@ using System.Timers;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Entities.TV;
 using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Subtitles;
 using MediaBrowser.Model.Entities;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Tasks;
@@ -24,14 +25,22 @@ class ActiveSubtitle
     public long Tick { get; set; }
 }
 
+class SessionData
+{
+    public long? Tick { get; set; }
+    public int? LastSubtitleIndex { get; set; }
+}
+
 public class SeekWatcher(
 IUserDataManager userDataManager,
     ISessionManager sessionManager,
-    ILogger<SeekWatcher> logger
+    ILogger<SeekWatcher> logger,
+    IUserManager userManager
 ) : IHostedService, IDisposable
 {
     private readonly IUserDataManager _userDataManager = userDataManager;
     private readonly ISessionManager _sessionManager = sessionManager;
+    private readonly IUserManager _userManager = userManager;
     private readonly ILogger<SeekWatcher> _logger = logger;
     private readonly System.Timers.Timer _playbackTimer = new(1000);
     // Contains the session ID and the timestamp at which the subtitles will be disabled
@@ -39,14 +48,44 @@ IUserDataManager userDataManager,
     private PluginConfiguration _config = new();
     // Tracking dictionary to see when a user skips back
     // When they do add to `_activeSubtitles`
-    private readonly ConcurrentDictionary<string, long?> _watchedSessions = new();
+    private readonly ConcurrentDictionary<string, SessionData> _watchedSessions = new();
 
+    private int? FindSubtitleStreamWithPredicate(IEnumerable<MediaStream> streams, Func<MediaStream, bool> predicate)
+    {
+        var stream = streams.FirstOrDefault(predicate);
+        return stream?.Index;
+    }
     private int TryFindSubtitleStream(SessionInfo session)
     {
         // Preference:
         // 1. Use the last-loaded subtitles
-        // 2. Try to match the user's language preference
-        // 3. Use the first available subtitles
+        // 2. Use the default subtitle stream
+        // 3. Use the user's preferred language
+
+        var streams = session.NowPlayingItem.MediaStreams.Where(stream => stream.Type == MediaStreamType.Subtitle);
+
+        var userLanguagePreference = _userManager.GetUserById(session.UserId)!.SubtitleLanguagePreference;
+
+        // Data guaranteed to exist
+        _watchedSessions.TryGetValue(session.Id, out var data);
+        var lastLoadedSubtitleIndex = FindSubtitleStreamWithPredicate(streams, stream => stream.Index == data!.LastSubtitleIndex);
+        if (lastLoadedSubtitleIndex.HasValue && lastLoadedSubtitleIndex.Value >= 0)
+        {
+            return lastLoadedSubtitleIndex.Value;
+        }
+
+        var defaultSubtitleIndex = FindSubtitleStreamWithPredicate(streams, stream => stream.IsDefault);
+        if (defaultSubtitleIndex.HasValue && defaultSubtitleIndex.Value >= 0)
+        {
+            return defaultSubtitleIndex.Value;
+        }
+
+        var preferredSubtitleIndex = FindSubtitleStreamWithPredicate(streams, stream => string.IsNullOrWhiteSpace(userLanguagePreference) || stream.Language == userLanguagePreference);
+        if (preferredSubtitleIndex.HasValue && preferredSubtitleIndex.Value >= 0)
+        {
+            return preferredSubtitleIndex.Value;
+        }
+
         return -1;
     }
 
@@ -59,24 +98,20 @@ IUserDataManager userDataManager,
             {
                 continue;
             }
+            _logger.LogTrace($"Session {session.Id} tick ({session.PlayState.SubtitleStreamIndex}, {session.PlayState.PositionTicks})");
 
             // If a session skipped back check for subtitles and enable them if necessary
-            if (session.PlayState.PositionTicks < _watchedSessions.GetOrAdd(session.Id, (_) => session.PlayState.PositionTicks))
+            var watchedSession = _watchedSessions.GetOrAdd(session.Id, (_) => new SessionData { LastSubtitleIndex = session.PlayState.SubtitleStreamIndex, Tick = session.PlayState.PositionTicks });
+            if (session.PlayState.PositionTicks is not null && session.PlayState.PositionTicks < watchedSession.Tick && session.PlayState.SubtitleStreamIndex == -1)
             {
-                // If subtitles are already enabled then don't do anything
-                if (session.PlayState.SubtitleStreamIndex != -1)
-                {
-                    continue;
-                }
-
-                // If subtitles are not enabled then enable them
+                // Since subtitles are not enabled, enable them
                 var index = TryFindSubtitleStream(session);
 
-                if (index != -1 && session.PlayState.PositionTicks is not null)
+                if (index != -1)
                 {
-                    _logger.LogDebug($"Found subtitles for {session.Id} on track {index}, enabling");
+                    _logger.LogDebug($"Found subtitles for {session.Id} on track {index}, enabling until {watchedSession.Tick}");
 
-                    _activeSubtitles.TryAdd(session.Id, new ActiveSubtitle { Tick = session.PlayState.PositionTicks.Value, Index = index });
+                    _activeSubtitles.TryAdd(session.Id, new ActiveSubtitle { Tick = watchedSession.Tick.Value, Index = index });
 
                     _sessionManager.SendGeneralCommand(session.Id, session.Id, new GeneralCommand
                     {
@@ -84,32 +119,41 @@ IUserDataManager userDataManager,
                         Arguments = { { "Index", index.ToString() } }
                     }, CancellationToken.None);
                 }
-            }
-
-            if (!_activeSubtitles.TryGetValue(session.Id, out var activeSubtitle))
-            {
-                continue;
-            }
-
-            if (activeSubtitle.Tick <= session.PlayState.PositionTicks)
-            {
-                _activeSubtitles.TryRemove(session.Id, out _);
-
-                // If the user changed subtitles between DejaVu turning them on and the off period being hit then don't take any action
-                if (session.PlayState.SubtitleStreamIndex != activeSubtitle.Index)
+                else
                 {
-                    continue;
+                    _logger.LogError($"Failed to find subtitles for {session.Id}");
                 }
+            }
 
-                _logger.LogInformation("Disabling subtitles for session {SessionId}", session.Id);
+            watchedSession!.Tick = session.PlayState.PositionTicks;
+            if (session.PlayState.SubtitleStreamIndex != -1)
+            {
+                watchedSession!.LastSubtitleIndex = session.PlayState.SubtitleStreamIndex;
+            }
 
-                _sessionManager.SendGeneralCommand(session.Id, session.Id, new GeneralCommand
+            if (_activeSubtitles.TryGetValue(session.Id, out var activeSubtitle))
+            {
+                if (activeSubtitle.Tick <= session.PlayState.PositionTicks)
                 {
-                    Name = GeneralCommandType.SetSubtitleStreamIndex,
-                    Arguments = {
-                        {"Index", "-1"}
+                    _activeSubtitles.TryRemove(session.Id, out _);
+
+                    // If the user changed subtitles between DejaVu turning them on and the off period being hit then don't take any action
+                    if (session.PlayState.SubtitleStreamIndex != activeSubtitle.Index)
+                    {
+                        _logger.LogDebug($"User changed subtitles during DejaVu period for session {session.Id} (SSI {session.PlayState.SubtitleStreamIndex} != {activeSubtitle.Index}), skipping");
+                        continue;
                     }
-                }, CancellationToken.None);
+
+                    _logger.LogDebug($"Disabling subtitles for session {session.Id}");
+
+                    _sessionManager.SendGeneralCommand(session.Id, session.Id, new GeneralCommand
+                    {
+                        Name = GeneralCommandType.SetSubtitleStreamIndex,
+                        Arguments = {
+                                        {"Index", "-1"}
+                                    }
+                    }, CancellationToken.None);
+                }
             }
         }
 
@@ -130,6 +174,7 @@ IUserDataManager userDataManager,
         _logger.LogDebug("Starting SeekWatcher");
 
         _playbackTimer.Elapsed += PlaybackTimerElapsed;
+        _playbackTimer.AutoReset = true;
         _playbackTimer.Start();
 
         return Task.CompletedTask;
